bbd1093ef5e4699183d0f33c11441fd3221739ca
ngorijanuario <68328526+ngorijanuario@users.noreply.github.com>
2020-07-16 05:12:55 +0100

    Iniciando com Variaveis
    Meu primeiro contacto com a linguagem Dart.
    No dia 1, estudei sobre constantes

---
+++ Variaveis.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Variaveis do tipo String
+  var nome = 'Ngori';
+  dynamic a = nome;
+  
+  //Declarando e manipulando constantes
+  const constante = ['Passado'];
+  // Variavel do tipo corda
+  var texto = 'Ola $nome ao mundo do Dart!';
+}
