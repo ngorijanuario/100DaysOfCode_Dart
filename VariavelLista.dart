22bc3205eb4e75a0cafec2415da875910e499a64
ngorijanuario <68328526+ngorijanuario@users.noreply.github.com>
2020-07-16 05:24:41 +0100

    Add files via upload

---
+++ VariavelLista.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Boolean type variable
+  var r = '';
+  assert(r.isEmpty);
+  
+  //Create lists
+  var a = [1,2,3,4];
+  assert(a.length == 4);
+  print(a[1]);
+  var b = [...a,5,6,7,8];
+  print(b);
+}
